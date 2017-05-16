012d8b585432db44ee382427c47ad4128f5eab0b
xxnan <vanke@vankedeMacBook-Pro.local>
2017-05-16 21:13:02 +0800

    frist commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+appcan uexDatabaseMgr3.0 android 插件，优化数据库事物在在子线程处理
---
+++ src/org/zywx/wbpalmstar/plugin/uexdatabasemgr/DataBaseVO.java
@@ -0,0 +1,18 @@
+package org.zywx.wbpalmstar.plugin.uexdatabasemgr;
+
+import java.io.Serializable;
+
+
+public class DataBaseVO implements Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String name;
+
+    public String id;
+
+    public int version=1;
+}
